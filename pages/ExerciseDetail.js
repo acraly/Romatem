08f1dea27ccf60b516fcfa7a80633672d1fb4363
aly <acer.aly123@gmail.com>
2024-01-30 13:02:20 +0300

    last update

--- pages/ExerciseDetail.js
+++ pages/ExerciseDetail.js
@@ -15,34 +15,43 @@ import WebView from 'react-native-webview';
 //  set_sayisi: 3,
 //  video:"https://metamy.b-cdn.net/media/video/1.D%C3%B6nem/Front-End_Devolopment/10_Veri_T%C3%BCrleri___JAVASCRIPT_Dersleri.mp4"}]
 
-const ExerciseDetail = () => {
-  const exercises = require("../helpers/myExercise.json");
-  console.log(exercises);
-    const route = useRoute();
+// const ExerciseDetail = () => {
+//   const exercises = require("../helpers/myExercise.json");
+//   console.log(exercises);
+//   const route = useRoute();
 
-  const { exercise } = route.params;
-  const video = React.useRef(null);
-  // const secondVideo = React.useRef(null);
-  const [status,setStatus]=React.useState({})
-  // const [statusSecondVideo,setStatusSecondVideo]=React.useState({})
+//   const { exercise } = route.params;
+//   const video = React.useRef(null);
+//   // const secondVideo = React.useRef(null);
+//   const [status, setStatus] = React.useState({})
+//   // const [statusSecondVideo,setStatusSecondVideo]=React.useState({})
 
 
-  return (
-    <ScrollView  contentContainerStyle={styles.container}>
-     <Video 
-     ref={video}
-<<<<<<< HEAD
-     source={{uri:"https://romatem.b-cdn.net/Denge%20%7C%20Uygulama%20Egzersizleri/Tandem.mp4"}}
-=======
-     style={styles.video}
-     source={{uri:"https://metamy.b-cdn.net/media/video/1.D%C3%B6nem/Front-End_Devolopment/Solunum_egzersizleri.mp4"}}
->>>>>>> f02f8c5ab0c87af3e0e3b6f5fd6bec244e40aa2a
-     useNativeControls
-     resizeMode='contain'
-     isLooping
-    //  onPlaybackStatusUpdate={setStatus}
-      />
-{/* <View style={styles.buttons}>
+//   return (
+//     <ScrollView contentContainerStyle={styles.container}>
+      {/* <Video
+        ref={video}
+        source={{ uri: "https://romatem.b-cdn.net/Denge%20%7C%20Uygulama%20Egzersizleri/Tandem.mp4" }}
+        style={styles.video}
+        source={{ uri: "https://metamy.b-cdn.net/media/video/1.D%C3%B6nem/Front-End_Devolopment/Solunum_egzersizleri.mp4" }}
+
+        useNativeControls
+        resizeMode='contain'
+        isLooping
+      //  onPlaybackStatusUpdate={setStatus}
+      /> */}
+      {/* <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://metamy.b-cdn.net/media/video/1.D%C3%B6nem/Front-End_Devolopment/Solunum_egzersizleri.mp4',
+        }}
+        useNativeControls
+        // resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      /> */}
+      {/* <View style={styles.buttons}>
         <Button
           title={status.isPlaying ? 'Pause' : 'Play'}
           onPress={() =>
@@ -50,29 +59,29 @@ const ExerciseDetail = () => {
           }
         />
       </View> */}
-      <Text style={styles.exerciseName}>{exercise.egzersiz_adi}</Text>
-      <Text style={styles.description}>{exercise.aciklama}</Text>
-      <Text style={styles.infoText}>Tekrar Sayısı: {exercise.tekrar_sayisi}</Text>
-      <Text style={styles.infoText}>Set Sayısı: {exercise.set_sayisi}</Text>
-    </ScrollView>
-  );
-};
+//       <Text style={styles.exerciseName}>{exercise.egzersiz_adi}</Text>
+//       <Text style={styles.description}>{exercise.aciklama}</Text>
+//       <Text style={styles.infoText}>Tekrar Sayısı: {exercise.tekrar_sayisi}</Text>
+//       <Text style={styles.infoText}>Set Sayısı: {exercise.set_sayisi}</Text>
+//     </ScrollView>
+//   );
+// };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'#F7DCCB',
+    backgroundColor: '#F7DCCB',
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 16,
     paddingHorizontal: 16,
   },
-  video:{
-    flex:1,
+  video: {
+    flex: 1,
     // flexDirection:'column',
     // justifyContent:'flex-start',
-    alignSelf:'stretch',
-   },
+    alignSelf: 'stretch',
+  },
   sliderContainer: {
     flexDirection: 'row',
     marginBottom: 16,
@@ -80,8 +89,8 @@ const styles = StyleSheet.create({
   sliderImage: {
     width: 350,
     height: 350,
-    marginRight:`auto`,
-    marginLeft:`auto`,
+    marginRight: `auto`,
+    marginLeft: `auto`,
     borderRadius: 8,
   },
   exerciseName: {
@@ -99,8 +108,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   wrapper: {
-    height:330,
-    textAlign:`center`
+    height: 330,
+    textAlign: `center`
   },
   backgroundVideo: {
     position: 'absolute',
@@ -111,5 +120,34 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ExerciseDetail;
+// export default ExerciseDetail;
+
+
+export default function ExerciseDetail() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://romatem.b-cdn.net/Solunum_Egzersizleri/B%C3%BCz%C3%BCk%20Dudak%20Solunumu.mp4',
+        }}
+        useNativeControls
+        // resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+    </View>
+  );
+}
 
--- pages/PatientCalendar.js
+++ pages/PatientCalendar.js
@@ -1,55 +1,19 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, Button, Platform } from 'react-native';
-import * as Calendar from 'expo-calendar';
+import React, {useState} from 'react';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 
-export default function PatientCalendar() {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Calendar.requestCalendarPermissionsAsync();
-      if (status === 'granted') {
-        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
-        console.log('Here are all your calendars:');
-        console.log({ calendars });
-      }
-    })();
-  }, []);
-  
+const PatientCalendar = () => {
+  const [selected, setSelected] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Calendar Module Example</Text>
-      <Button title="Create a new calendar"  />
-    </View>
+    <Calendar
+      onDayPress={day => {
+        setSelected(day.dateString);
+      }}
+      markedDates={{
+        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+      }}
+    />
   );
-}
+};
 
-async function getDefaultCalendarSource() {
-  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
-  return defaultCalendar.source;
-}
-
-async function createCalendar() {
-  const defaultCalendarSource =
-    Platform.OS === 'ios'
-      ? await getDefaultCalendarSource()
-      : { isLocalAccount: true, name: 'Expo Calendar' };
-  const newCalendarID = await Calendar.createCalendarAsync({
-    title: 'Expo Calendar',
-    color: 'blue',
-    entityType: Calendar.EntityTypes.EVENT,
-    sourceId: defaultCalendarSource.id,
-    source: defaultCalendarSource,
-    name: 'internalCalendarName',
-    ownerAccount: 'personal',
-    accessLevel: Calendar.CalendarAccessLevel.OWNER,
-  });
-  console.log(`Your new calendar ID is: ${newCalendarID}`);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-});
+export default PatientCalendar;
